56ff8e13fd9fa1c93d7053ee1cd8fdc802ab5f02
Ace <aevancho@me.com>
2024-04-12 11:34:05 -0500

    add mock apis

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,6 @@
+import dotenv from 'dotenv';
 import { processFilesInDirectory } from "../utils/utils.js";
+dotenv.config();
 
 export const mockAPICall = async () => {
     const directoryPath = 'src/data/db/sessions/1059303213';
@@ -10,3 +12,61 @@ export const mockAPICall = async () => {
     }
     return data;
 };
+
+async function postLTISession(): Promise<string> {
+    const url = 'https://staging.proctoru.com/api/secure_browser/lti_sessions';
+    const headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': process.env.GUARDIAN_USER_AGENT!,
+    };
+
+    const ltiPayload = process.env.LTI_PAYLOAD!;
+    const payloadObject = JSON.parse(ltiPayload);
+    const body = JSON.stringify(payloadObject);
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: body
+        });
+
+        let cookies = response['headers'].getSetCookie();
+        const formattedCookies =  cookies.slice(1,3).join(',');
+        return formattedCookies;
+    } catch (error) {
+        console.error('Error making POST request:', error);
+        throw new Error('Error making POST request');
+    }
+}
+
+async function getFulfillmentDetails(cookies: string, uuid: string): Promise<any> {
+    const url = `https://staging.proctoru.com/api/secure_browser/fulfillments/${uuid}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Cookie': cookies,
+                'User-Agent': process.env.GUARDIAN_USER_AGENT!
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error making GET request:', error);
+        throw new Error('Error making GET request');
+    }
+}
+
+async function getReservationData(uuid: string) {
+    try {
+        const cookies = await postLTISession();
+        const details = await getFulfillmentDetails(cookies, uuid);
+        console.log(details);
+    } catch (error) {
+        console.error('Failed:', error);
+    }
+}
+
+getReservationData(process.env.FULFILLMENT_UUID!)
